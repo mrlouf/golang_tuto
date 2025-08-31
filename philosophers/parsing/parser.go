8bfad340e5fe2a2bbc5fedba9b9e6dad82618ac7
Nicolas <ponchon.nicolas@gmail.com>
2025-08-31 17:27:53 +0200

    parsing the number of philosophers

--- philosophers/parsing/parser.go
+++ philosophers/parsing/parser.go
@@ -2,10 +2,22 @@ package parsing
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func Parser(args []string) {
 	fmt.Println("Starting Parser...")
-
 	fmt.Println(args)
+
+	tmp, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Invalid number format")
+		os.Exit(1)
+	}
+
+	if tmp < 1 || tmp > 200 {
+		fmt.Println("Invalid number of philosophers (must be 1-200)")
+		os.Exit(1)
+	}
 }
